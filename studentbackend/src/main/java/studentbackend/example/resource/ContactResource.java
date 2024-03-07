ff301c5cc8f45cbcd6d6ac2ef2d40e5e02a6b8d8
Neema <patelneema93@gmail.com>
2024-03-07 11:47:27 -0500

    create postmapping

--- studentbackend/src/main/java/studentbackend/example/resource/ContactResource.java
+++ studentbackend/src/main/java/studentbackend/example/resource/ContactResource.java
@@ -1,13 +1,27 @@
 package studentbackend.example.resource;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import studentbackend.example.domain.Contact;
+import studentbackend.example.service.ContactService;
 
 @RestController
 @RequestMapping("/contacts")
 @RequiredArgsConstructor
 public class ContactResource {
+    private final ContactService contactService;
+       @PostMapping
+    public ResponseEntity<Contact> createContact(@RequestBody Contact contact) {
+        //return ResponseEntity.ok().body(contactService.createContact(contact));
+        return ResponseEntity.created(URI.create("/contacts/userID")).body(contactService.createContact(contact));
+    }
+
     
 }
