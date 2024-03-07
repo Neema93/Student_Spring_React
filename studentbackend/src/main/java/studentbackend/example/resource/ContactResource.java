fcf6ae007e388bd8c1e611030116662e3df5730a
Neema <patelneema93@gmail.com>
2024-03-07 11:44:37 -0500

    create resource file

---
+++ studentbackend/src/main/java/studentbackend/example/resource/ContactResource.java
@@ -0,0 +1,13 @@
+package studentbackend.example.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/contacts")
+@RequiredArgsConstructor
+public class ContactResource {
+    
+}
