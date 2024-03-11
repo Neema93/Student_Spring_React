9f8ce2f82384320ad0c26b3f4d586d3f65e7cd72
Neema <patelneema93@gmail.com>
2024-03-11 15:41:24 -0400

    (exclude = {DataSourceAutoConfiguration.class }) add in springboot application

--- studentbackend/src/main/java/studentbackend/example/studentbackend/StudentbackendApplication.java
+++ studentbackend/src/main/java/studentbackend/example/studentbackend/StudentbackendApplication.java
@@ -2,8 +2,10 @@ package studentbackend.example.studentbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class StudentbackendApplication {
 
 	public static void main(String[] args) {
