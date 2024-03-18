02c781805790c514ebf2f8f7a49b5b3a367780af
Neema <patelneema93@gmail.com>
2024-03-18 11:22:07 -0400

    change port

--- studentbackend/src/main/java/studentbackend/example/resource/ContactResource.java
+++ studentbackend/src/main/java/studentbackend/example/resource/ContactResource.java
@@ -1,32 +1,19 @@
 package studentbackend.example.resource;
-
-
-
 import lombok.RequiredArgsConstructor;
 import studentbackend.example.domain.Contact;
 import studentbackend.example.service.ContactService;
-
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
-
 import java.net.URI;
-
-
-
-
-
 @RestController
-@RequestMapping("/contacts")
+@RequestMapping("")
 @RequiredArgsConstructor
 public class ContactResource {
     private final ContactService contactService;
@@ -46,11 +33,5 @@ public class ContactResource {
     @GetMapping("/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable(value = "id") String id) {
         return ResponseEntity.ok().body(contactService.getContact(id));
-    }
-
-    
-
-
-
-   
+    }   
 }
