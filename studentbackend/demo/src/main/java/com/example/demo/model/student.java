0a5bdeb93158113fe32ea52e208c843a31fb5980
Neema <patelneema93@gmail.com>
2024-12-25 17:07:41 -0500

    ctrate student controller

--- studentbackend/demo/src/main/java/com/example/demo/model/student.java
+++ studentbackend/demo/src/main/java/com/example/demo/model/student.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class student {
+public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long studentId;
