df1e3eb3aa1d2d076d923727a7647ac7e33df5f9
Neema <patelneema93@gmail.com>
2025-02-05 13:55:25 -0500

    set link in Heading Page

--- studentforntend/src/Componet/AppRouter.jsx
+++ studentforntend/src/Componet/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import StudentForm from './StudentForm';
 import StudentDetails from './StudentDetails';
+import Heading from './Heading';
 const AppRouter = () => {
     return (
         <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Heading" element={<Heading />} />
           <Route path="/StudentForm" element={<StudentForm />} />
           <Route path="/StudentDetails" element={<StudentDetails />} />
           
--- studentforntend/src/Componet/Heading.jsx
+++ studentforntend/src/Componet/Heading.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Heading = () => {
  return (
     <>
     <h1> Student </h1>
+    <Link to="/">Home</Link>
+    <Link to="/StudentForm">StudentForm</Link>
     </>
  )
 }
