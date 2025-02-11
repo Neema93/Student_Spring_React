4f3bb84e7c6dcb7c2269555f079b7a0463d9f900
Neema <patelneema93@gmail.com>
2025-02-11 10:14:21 -0500

    set heading in approute

--- studentforntend/src/Componet/AppRouter.jsx
+++ studentforntend/src/Componet/AppRouter.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import StudentForm from './StudentForm';
-import StudentDetails from './StudentDetails';
-import Heading from './Heading';
-import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StudentForm from "./StudentForm";
+import StudentDetails from "./StudentDetails";
+import Heading from "./Heading";
+import Home from "./Home";
 const AppRouter = () => {
-    return (
-        <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Heading" element={<Heading />} />
-          <Route path="/StudentForm" element={<StudentForm />} />
-          <Route path="/StudentDetails" element={<StudentDetails />} />
-          
-        </Routes>
-      </Router>
-    )
-}
+  return (
+    <Router>
+      <Heading />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Heading" element={<Heading />} />
+        <Route path="/StudentForm" element={<StudentForm />} />
+        <Route path="/StudentDetails" element={<StudentDetails />} />
+      </Routes>
+    </Router>
+  );
+};
 export default AppRouter;
